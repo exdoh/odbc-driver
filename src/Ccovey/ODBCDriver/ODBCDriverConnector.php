c1b077c52f4c6f0295aec906f2d56fecac85ac0e
Gritsakorn <raiderex.7@gmail.com>
2016-01-26 11:57:17 +0700

    Update ODBCDriverConnector.php
    edit return dsn

--- src/Ccovey/ODBCDriver/ODBCDriverConnector.php
+++ src/Ccovey/ODBCDriver/ODBCDriverConnector.php
@@ -21,7 +21,8 @@ class ODBCDriverConnector extends Connector implements ConnectorInterface
 	protected function getDsn(array $config) {
         extract($config);
 
-        $dsn = "odbc:{$dsn}; {$username}; {$password};";
+        $dsn = "odbc:{$dsn};DATABASE={$database};HOSTNAME={$hostname};PORT={$port};PROTOCOL={$protocal};CharSet={$charset};
+               {$username}; {$password};";
 
         return $dsn;
     }
